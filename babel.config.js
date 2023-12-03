226ac21435068b8966980c4cede41be0551f49f1
Ali Khan <77904698+alikhan1998@users.noreply.github.com>
2023-12-03 15:53:17 +0500

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,19 +1,20 @@
 module.exports = {
-  babelrcRoots: [babel.config.js
+  babelrcRoots: [
+    'babel.config.js'
   ],
   parserOpts: {
     allowReturnOutsideFunction: true
   },
-  presets: [@babel/preset-env
+  presets: [
     [
-      "@babel/preset-env",
+      '@babel/preset-env',
       {
-        "exclude": [
-          "@babel/plugin-transform-regenerator",
-          "@babel/plugin-transform-async-to-generator"
+        exclude: [
+          '@babel/plugin-transform-regenerator',
+          '@babel/plugin-transform-async-to-generator'
         ]
       }
     ],
-    "@babel/preset-react"
+    '@babel/preset-react'
   ]
 };
